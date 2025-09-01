ce53feac90800dbac66c228591eb484fb8f8a9e6
Adrian Steven Narea <anarea@telconet.ec>
2025-09-01 14:29:02 -0500

    Vista /pantalla para mostrar foto por codigo AB#1

--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,13 @@
 
 from django.contrib import admin
+from comedor.views import pantalla_view
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pantalla/', pantalla_view),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
